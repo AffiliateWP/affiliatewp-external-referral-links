7f003ab30944f7733afcadecbae6774c7c7affce
David Beja <david.beja@gmail.com>
2021-06-23 04:07:09 +0100

    issue/15 - Allow language translations to be handled by WP repo (#28)
    Co-authored-by: Drew Jaynes <DrewAPicture@users.noreply.github.com>
    Co-authored-by: Drew Jaynes <drew@affiliatewp.com>
    
    Fixes #15

--- affiliatewp-external-referral-links.php
+++ affiliatewp-external-referral-links.php
@@ -7,7 +7,6 @@
  * Author URI: https://sandhillsdev.com
  * Version: 1.0.2
  * Text Domain: affiliatewp-external-referral-links
- * Domain Path: languages
  *
  * AffiliateWP is distributed under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 2 of the License, or
@@ -60,7 +59,6 @@ final class AffiliateWP_External_Referral_Links {
 			self::$plugin_url = plugin_dir_url( __FILE__ );
 			self::$version    = '1.0.2';
 
-			self::$instance->load_textdomain();
 			self::$instance->includes();
 			self::$instance->hooks();
 
@@ -94,39 +92,6 @@ final class AffiliateWP_External_Referral_Links {
 	public function __wakeup() {
 		// Unserializing instances of the class is forbidden
 		_doing_it_wrong( __FUNCTION__, __( 'Cheatin&#8217; huh?', 'affiliatewp-external-referral-links' ), '1.0' );
-	}
-
-	/**
-	 * Loads the plugin language files
-	 *
-	 * @access public
-	 * @since 1.0
-	 * @return void
-	 */
-	public function load_textdomain() {
-
-		// Set filter for plugin's languages directory
-		$lang_dir = dirname( plugin_basename( __FILE__ ) ) . '/languages/';
-		$lang_dir = apply_filters( 'aff_wp_languages_directory', $lang_dir );
-
-		// Traditional WordPress plugin locale filter
-		$locale   = apply_filters( 'plugin_locale',  get_locale(), 'affiliatewp-external-referral-links' );
-		$mofile   = sprintf( '%1$s-%2$s.mo', 'affiliatewp-external-referral-links', $locale );
-
-		// Setup paths to current locale file
-		$mofile_local  = $lang_dir . $mofile;
-		$mofile_global = WP_LANG_DIR . '/affiliatewp-external-referral-links/' . $mofile;
-
-		if ( file_exists( $mofile_global ) ) {
-			// Look in global /wp-content/languages/affiliatewp-external-referral-links/ folder
-			load_textdomain( 'affiliatewp-external-referral-links', $mofile_global );
-		} elseif ( file_exists( $mofile_local ) ) {
-			// Look in local /wp-content/plugins/affiliatewp-external-referral-links/languages/ folder
-			load_textdomain( 'affiliatewp-external-referral-links', $mofile_local );
-		} else {
-			// Load the default language files
-			load_plugin_textdomain( 'affiliatewp-external-referral-links', false, $lang_dir );
-		}
 	}
 
 	/**
