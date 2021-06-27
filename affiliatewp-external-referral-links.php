f0c271f67bc1fae7f2df67b28c036d8f2e03ee7a
Drew Jaynes <drew@affiliatewp.com>
2021-06-27 16:38:24 -0600

    Recreate main plugin file, swap in the main plugin header. #32

---
+++ affiliatewp-external-referral-links.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Plugin Name: AffiliateWP - External Referral Links
+ * Plugin URI: https://affiliatewp.com
+ * Description: Allows you to promote external landing pages/sites with the affiliate ID or username appended to the URLs.
+ * Author: Sandhills Development, LLC
+ * Author URI: https://sandhillsdev.com
+ * Version: 1.0.2
+ * Text Domain: affiliatewp-external-referral-links
+ *
+ * AffiliateWP is distributed under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * any later version.
+ *
+ * AffiliateWP is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with AffiliateWP. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package AffiliateWP External Referral Links
+ * @category Core
+ * @version 1.0.2
+ */
+
+
--- includes/class-affiliatewp-external-referral-links.php
+++ includes/class-affiliatewp-external-referral-links.php
@@ -1,24 +1,12 @@
 <?php
 /**
- * Plugin Name: AffiliateWP - External Referral Links
- * Plugin URI: https://affiliatewp.com
- * Description: Allows you to promote external landing pages/sites with the affiliate ID or username appended to the URLs.
- * Author: Sandhills Development, LLC
- * Author URI: https://sandhillsdev.com
- * Version: 1.0.2
- * Text Domain: affiliatewp-external-referral-links
+ * Core: Plugin Bootstrap
  *
- * AffiliateWP is distributed under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * any later version.
- *
- * AffiliateWP is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with AffiliateWP. If not, see <http://www.gnu.org/licenses/>.
+ * @package     AffiliateWP External Referral Links
+ * @subpackage  Core
+ * @copyright   Copyright (c) 2021, Sandhills Development, LLC
+ * @license     http://opensource.org/licenses/gpl-2.0.php GNU Public License
+ * @since       1.1
  */
 
 // Exit if accessed directly
