cf4bea7274448d6daddd1e8d30b6fa130e86211c
Drew Jaynes <DrewAPicture@users.noreply.github.com>
2021-06-28 10:21:37 -0600

    issue/32 – Bundle affwp-addon-tools and implement requirement checks (#35)
    Fixes #32

--- affiliatewp-external-referral-links.php
+++ affiliatewp-external-referral-links.php
@@ -19,220 +19,154 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with AffiliateWP. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package AffiliateWP External Referral Links
+ * @category Core
+ * @version 1.0.2
  */
 
 // Exit if accessed directly
-if ( ! defined( 'ABSPATH' ) ) exit;
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
 
-final class AffiliateWP_External_Referral_Links {
-
-	/** Singleton *************************************************************/
-
-	/**
-	 * @var AffiliateWP_External_Referral_Links The one true AffiliateWP_External_Referral_Links
-	 * @since 1.0
-	 */
-	private static $instance;
-
-	public static  $plugin_dir;
-	public static  $plugin_url;
-	private static $version;
-	private        $expiration_time;
-
-	/**
-	 * Main AffiliateWP_External_Referral_Links Instance
-	 *
-	 * Insures that only one instance of AffiliateWP_External_Referral_Links exists in memory at any one
-	 * time. Also prevents needing to define globals all over the place.
-	 *
-	 * @since 1.0
-	 * @static
-	 * @staticvar array $instance
-	 * @return The one true AffiliateWP_External_Referral_Links
-	 */
-	public static function instance() {
-		if ( ! isset( self::$instance ) && ! ( self::$instance instanceof AffiliateWP_External_Referral_Links ) ) {
-
-			self::$instance   = new AffiliateWP_External_Referral_Links;
-
-			self::$plugin_dir = plugin_dir_path( __FILE__ );
-			self::$plugin_url = plugin_dir_url( __FILE__ );
-			self::$version    = '1.0.2';
-
-			self::$instance->includes();
-			self::$instance->hooks();
-
-		}
-
-		return self::$instance;
-	}
-
-	/**
-	 * Throw error on object clone
-	 *
-	 * The whole idea of the singleton design pattern is that there is a single
-	 * object therefore, we don't want the object to be cloned.
-	 *
-	 * @since 1.0
-	 * @access protected
-	 * @return void
-	 */
-	public function __clone() {
-		// Cloning instances of the class is forbidden
-		_doing_it_wrong( __FUNCTION__, __( 'Cheatin&#8217; huh?', 'affiliatewp-external-referral-links' ), '1.0' );
-	}
-
-	/**
-	 * Disable unserializing of the class
-	 *
-	 * @since 1.0
-	 * @access protected
-	 * @return void
-	 */
-	public function __wakeup() {
-		// Unserializing instances of the class is forbidden
-		_doing_it_wrong( __FUNCTION__, __( 'Cheatin&#8217; huh?', 'affiliatewp-external-referral-links' ), '1.0' );
-	}
-
-	/**
-	 * Include necessary files
-	 *
-	 * @access      private
-	 * @since       1.0.0
-	 * @return      void
-	 */
-	private function includes() {
-		if ( is_admin() ) {
-			// admin page
-			require_once self::$plugin_dir . 'includes/admin.php';
-		}
-	}
-
-	/**
-	 * Setup the default hooks and actions
-	 *
-	 * @since 1.0
-	 *
-	 * @return void
-	 */
-	private function hooks() {
-
-		// load scripts.
-		add_action( 'wp_enqueue_scripts', array( $this, 'load_scripts' ) );
-
-		// plugin meta.
-		add_filter( 'plugin_row_meta', array( $this, 'plugin_meta' ), null, 2 );
-
-		// filter the custom cookie name.
-		add_filter( 'affwp_get_cookie_name', array( $this, 'set_cookie_name' ), 10, 2 );
-
-	}
-
-	/**
-	 * Get options
-	 *
-	 * @since 1.0
-	 */
-	private function get_option( $option = '' ) {
-		$options = get_option( 'affiliatewp_external_referral_links' );
-
-		if ( ! isset( $option ) )
-			return;
-
-		return $options[$option];
-	}
-
-
-	/**
-	 * Get the cookie expiration time in days
-	 *
-	 * @since 1.0
-	 */
-	public function get_expiration_time() {
-		return apply_filters( 'affwp_erl_cookie_expiration', $this->get_option( 'cookie_expiration' ) );
-	}
-
-	/**
-	 * Load JS files
-	 *
-	 * @since 1.0
-	 */
-	public function load_scripts() {
-
-		// return if no URL is set
-		if ( ! $this->get_option('url') ) {
-			return;
-		}
-
-		wp_enqueue_script( 'affwp-erl', self::$plugin_url . 'assets/js/affwp-external-referral-links.min.js', array( 'jquery' ), self::$version );
-
-		// get cookie name.
-		$affwp_version = defined( 'AFFILIATEWP_VERSION' ) ? AFFILIATEWP_VERSION : 'undefined';
-		if ( version_compare( $affwp_version, '2.7.1', '>=' ) ) {
-			$cookie = affiliate_wp()->tracking->get_cookie_name( 'erl-affiliate' );
-		} else {
-			$cookie = 'affwp_erl_id';
-		}
-
-		wp_localize_script( 'affwp-erl', 'affwp_erl_vars', array(
-			'cookie_expiration' => $this->get_expiration_time(),
-			'referral_variable' => $this->get_option( 'referral_variable' ),
-			'url'               => $this->get_option( 'url' ),
-			'cookie'            => $cookie,
-		));
-
-	}
-
-	/**
-	 * Sets the custom cookie name
-	 *
-	 * @param string $cookie_name The cookie name.
-	 * @param string $cookie_type The cookie type.
-	 * @return string The final cookie name.
-	 */
-	public function set_cookie_name( $cookie_name, $cookie_type ) {
-		if ( 'erl-affiliate' === $cookie_type ) {
-			$cookie_name = 'affwp_erl_id';
-		}
-
-		return $cookie_name;
-	}
-
-	/**
-	 * Modify plugin metalinks
-	 *
-	 * @access      public
-	 * @since       1.0
-	 * @param       array $links The current links array
-	 * @param       string $file A specific plugin table entry
-	 * @return      array $links The modified links array
-	 */
-	public function plugin_meta( $links, $file ) {
-	    if ( $file == plugin_basename( __FILE__ ) ) {
-	        $plugins_link = array(
-	            '<a title="' . __( 'Get more add-ons for AffiliateWP', 'affiliatewp-external-referral-links' ) . '" href="http://affiliatewp.com/addons/" target="_blank">' . __( 'Get add-ons', 'affiliatewp-external-referral-links' ) . '</a>'
-	        );
-
-	        $links = array_merge( $links, $plugins_link );
-	    }
-
-	    return $links;
-	}
+if ( ! class_exists( 'AffiliateWP_Requirements_Check' ) ) {
+	require_once dirname( __FILE__ ) . '/includes/lib/affwp/class-affiliatewp-requirements-check.php';
 }
 
 /**
- * The main function responsible for returning the one true AffiliateWP_External_Referral_Links
- * Instance to functions everywhere.
+ * Class used to check requirements for and bootstrap the plugin.
  *
- * Use this function like you would a global variable, except without needing
- * to declare the global.
+ * @since 1.1
  *
- * Example: <?php $affiliatewp_external_referral_links = affiliatewp_external_referral_links(); ?>
- *
- * @since 1.0
- * @return object The one true AffiliateWP_External_Referral_Links Instance
+ * @see Affiliate_WP_Requirements_Check
  */
-function affiliatewp_external_referral_links() {
-     return AffiliateWP_External_Referral_Links::instance();
+class AffiliateWP_ERL_Requirements_Check extends AffiliateWP_Requirements_Check {
+
+	/**
+	 * Plugin slug.
+	 *
+	 * @since 1.1
+	 * @var   string
+	 */
+	protected $slug = 'affiliatewp-external-referral-links';
+
+	/**
+	 * Add-on requirements.
+	 *
+	 * @since 1.1
+	 * @var   array[]
+	 */
+	protected $addon_requirements = array(
+		// AffiliateWP.
+		'affwp' => array(
+			'minimum' => '2.6',
+			'name'    => 'AffiliateWP',
+			'exists'  => true,
+			'current' => false,
+			'checked' => false,
+			'met'     => false
+		),
+	);
+
+	/**
+	 * Bootstrap everything.
+	 *
+	 * @since 1.1
+	 */
+	public function bootstrap() {
+		if ( ! class_exists( 'Affiliate_WP' ) ) {
+
+			if ( ! class_exists( 'AffiliateWP_Activation' ) ) {
+				require_once 'includes/lib/affwp/class-affiliatewp-activation.php';
+			}
+
+			// AffiliateWP activation
+			if ( ! class_exists( 'Affiliate_WP' ) ) {
+				$activation = new AffiliateWP_Activation( plugin_dir_path( __FILE__ ), basename( __FILE__ ) );
+				$activation = $activation->run();
+			}
+		} else {
+			\AffiliateWP_External_Referral_Links::instance( __FILE__ );
+		}
+	}
+
+	/**
+	 * Loads the add-on.
+	 *
+	 * @since 1.1
+	 */
+	protected function load() {
+		// Maybe include the bundled bootstrapper.
+		if ( ! class_exists( 'AffiliateWP_External_Referral_Links' ) ) {
+			require_once dirname( __FILE__ ) . '/includes/class-affiliatewp-external-referral-links.php';
+		}
+
+		// Maybe hook-in the bootstrapper.
+		if ( class_exists( 'AffiliateWP_External_Referral_Links' ) ) {
+
+			$affwp_version = get_option( 'affwp_version' );
+
+			if ( version_compare( $affwp_version, '2.7', '<' ) ) {
+				add_action( 'plugins_loaded', array( $this, 'bootstrap' ), 100 );
+			} else {
+				add_action( 'affwp_plugins_loaded', array( $this, 'bootstrap' ), 100 );
+			}
+
+			// Register the activation hook.
+			register_activation_hook( __FILE__, array( $this, 'install' ) );
+		}
+	}
+
+	/**
+	 * Install, usually on an activation hook.
+	 *
+	 * @since 1.1
+	 */
+	public function install() {
+		// Bootstrap to include all of the necessary files
+		$this->bootstrap();
+
+		if ( defined( 'AFFWP_ERL_VERSION' ) ) {
+			update_option( 'affwp_erl_version', AFFWP_ERL_VERSION );
+		}
+	}
+
+	/**
+	 * Plugin-specific aria label text to describe the requirements link.
+	 *
+	 * @since 1.1
+	 *
+	 * @return string Aria label text.
+	 */
+	protected function unmet_requirements_label() {
+		return esc_html__( 'AffiliateWP - External Referral Links Requirements', 'affiliatewp-external-referral-links' );
+	}
+
+	/**
+	 * Plugin-specific text used in CSS to identify attribute IDs and classes.
+	 *
+	 * @since 1.1
+	 *
+	 * @return string CSS selector.
+	 */
+	protected function unmet_requirements_name() {
+		return 'affiliatewp-external-referral-links-requirements';
+	}
+
+	/**
+	 * Plugin specific URL for an external requirements page.
+	 *
+	 * @since 1.1
+	 *
+	 * @return string Unmet requirements URL.
+	 */
+	protected function unmet_requirements_url() {
+		return 'https://docs.affiliatewp.com/article/2361-minimum-requirements-roadmaps';
+	}
+
 }
-add_action( 'plugins_loaded', 'affiliatewp_external_referral_links', 100 );
+
+$requirements = new AffiliateWP_ERL_Requirements_Check( __FILE__ );
+
+$requirements->maybe_load();
